fa6dc6612cf40be02505022c6e7c3b9f5721d9d4
xyzuan <xyzuannihboss@gmail.com>
2021-08-15 11:35:00 +0700

    xdroidUI: configs: Organize xdroidUI Configs

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2020 The xdroidOS Project
+# Copyright (C) 2021 xdroid, and xyzprjkt
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,67 +17,14 @@ LOCAL_PATH := $(call my-dir)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-# Offline charger
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    product_charger_res_images
+# xdroidUI Overlays
+include $(LOCAL_PATH)/configs/xdroidui_overlays.mk
 
-# Blur
-ifeq ($(XDROID_UI_BLUR), true)
-$(warning "xdroidUI: BlurUI has been enabled.")
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.sf.blurs_are_expensive=1 \
-    ro.surface_flinger.supports_background_blur=1
-endif
+# xdroidUI Packages
+include $(LOCAL_PATH)/configs/xdroidui_packages.mk
 
-# build.prop entrys
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.wallpapers_loc_request_suw=true
+# xdroidUI Props
+include $(LOCAL_PATH)/configs/xdroidui_props.mk
 
-# SetupWizard
-PRODUCT_PRODUCT_PROPERTIES += \
-    setupwizard.enable_assist_gesture_training=true \
-    setupwizard.feature.baseline_setupwizard_enabled=true \
-    setupwizard.feature.show_pixel_tos=true \
-    setupwizard.feature.show_support_link_in_deferred_setup=false \
-    setupwizard.theme=glif_v3_light
-
-# Gestures
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
-
-# IME
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.ime.bs_theme=true \
-    ro.com.google.ime.system_lm_dir=/system/product/usr/share/ime/google/d3_lms \
-    ro.com.google.ime.theme_id=5
-
-# Include package overlays
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/xdroidui/overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    vendor/xdroidui/overlay/common/
-
-# Apps
-PRODUCT_PACKAGES += \
-    NexusLauncherRelease \
-    PixelThemesStub \
-    ThemePicker \
-    WallpaperPickerGoogleRelease
-
-# Plugins
-include packages/apps/Plugins/plugins.mk
-
-# xd. Bootanimation
-ifeq ($(XDROID_BOOT_DARK),true)
-    $(warning "xdroidUI: Using Dark xd. Bootanimation")
-    PRODUCT_COPY_FILES += vendor/xdroidui/bootanimation/xd_boot_dark.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-    PRODUCT_COPY_FILES += vendor/xdroidui/bootanimation/xd_boot_dark.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
-else
-    $(warning "xdroidUI: Using Light xd. Bootanimation")
-    PRODUCT_COPY_FILES += vendor/xdroidui/bootanimation/xd_boot_light.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-    PRODUCT_COPY_FILES += vendor/xdroidui/bootanimation/xd_boot_light.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
-endif
-
-# Copy files
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xdroidui/etc,$(TARGET_COPY_OUT_SYSTEM_EXT)/etc)
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xdroidui/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+# xdroidUI Ui
+include $(LOCAL_PATH)/configs/xdroidui_ui.mk
---
+++ configs/xdroidui_ui.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2021 xdroid, and xyzprjkt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# xd. Bootanimation
+ifeq ($(XDROID_BOOT_DARK),true)
+    $(warning "xdroidUI: Using Dark xd. Bootanimation")
+    PRODUCT_COPY_FILES += vendor/xdroidui/bootanimation/xd_boot_dark.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+    PRODUCT_COPY_FILES += vendor/xdroidui/bootanimation/xd_boot_dark.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
+else
+    $(warning "xdroidUI: Using Light xd. Bootanimation")
+    PRODUCT_COPY_FILES += vendor/xdroidui/bootanimation/xd_boot_light.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+    PRODUCT_COPY_FILES += vendor/xdroidui/bootanimation/xd_boot_light.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
+endif
+
+# Copy files
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xdroidui/etc,$(TARGET_COPY_OUT_SYSTEM_EXT)/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xdroidui/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
